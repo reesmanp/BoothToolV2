91d7be03f34f2bb5b4ac899d4b01ab1d3533496e
reesmanp <paul.r.reesman@gmail.com>
2017-02-16 13:30:07 -0800

    STABLE: Modularized LESS and added LESS rules.

--- src/components/base.js
+++ src/components/base.js
@@ -1,14 +1,58 @@
 'use strict';
 
-require('../less/main.less');
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Login from './login';
+import Dashboard from './dashboard';
+require('../less/main.less');
 
 document.title = 'Booth Tool ' + require('../../package.json').version;
 
-ReactDOM.render(
-  <Login />,
+class Base extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      loggedIn: false
+    };
+    this.logIn = this.logIn.bind(this);
+    this.renderBackground = this.renderBackground.bind(this);
+  }
+
+  logIn () {
+    this.setState({
+      loggedIn: true
+    });
+  }
+
+  renderBackground (ele) {
+    return (
+      <div className='BackgroundColor-bg'>
+        <div className='BackgroundColor-fg'>
+          {ele}
+        </div>
+      </div>
+    );
+  }
+
+  render () {
+    return (
+      !this.state.loggedIn
+        ? this.renderBackground(<Login logIn={this.logIn}/>)
+        : this.renderBackground(this.props.children)
+    );
+  }
+}
+
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path='/Users/preesm/git/electron/src/html/index.html' component={Base}>
+      <IndexRoute component={Dashboard}>
+      </IndexRoute>
+    </Route>
+  </Router>
+  ),
   document.getElementById('content')
 );
 
--- src/components/login.js
+++ src/components/login.js
@@ -7,13 +7,32 @@ class Login extends Component {
     this.state = {
       username: null
     };
+    this.authenticate = this.authenticate.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  authenticate () {
+    const {
+      logIn
+    } = this.props;
+
+    this.state.username ? logIn() : null;
+  }
+
+  update (evt) {
+    this.setState({
+      username: evt.target.value
+    });
   }
 
   render () {
     return (
       <div className='loginDiv'>
         <h1>Login</h1>
-        <span>Username: <input type='text' name='username' autoFocus='true' placeholder='Username'/></span>
+        <span>Username:
+          <input type='text' name='username' autoFocus='true' placeholder='Username' onBlur={this.update}/>
+        </span>
+        <button type='button' onClick={this.authenticate}>Log In</button>
       </div>
     );
   }
